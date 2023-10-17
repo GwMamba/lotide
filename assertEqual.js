a76ea3b5e2ba7a6654e1cf63db81da80cf47339f
Gerald <gerald.mwang@gmail.com>
2023-10-17 16:20:02 -0600

    assertEqual corrected with test cases

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,14 @@
 // Function Implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(":) Assertion Passed:", actual, "===", expected);
+    console.log("✅ Assertion Passed:", actual, "==", expected);
   } else {
-    console.log(":( Assertion Failed:", actual, "!==", expected);
+    console.log("🛑 Assertion Failed:", actual, "!=", expected);
   }
 };
 
 // Test code
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual(5, 25);
+assertEqual("Alberta", "Ontario")
