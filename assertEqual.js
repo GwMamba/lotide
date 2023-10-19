e7cdb5b20da93f0393a4a433cffe1e99a604bdbb
Gerald <gerald.mwang@gmail.com>
2023-10-18 20:26:19 -0600

    Corrected earlier mistake.

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // Function Implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅ Assertion Passed:", actual, "==", expected);
+    console.log("✅✅✅ Assertion Passed:", actual, "===", expected);
   } else {
-    console.log("🛑 Assertion Failed:", actual, "!=", expected);
+    console.log("🛑🛑🛑 Assertion Failed:", actual, "!==", expected);
   }
 };
 
@@ -12,3 +12,4 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(5, 25);
 assertEqual("Alberta", "Ontario")
+assertEqual("X56", "X56")
