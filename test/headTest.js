03db9d040f3b978f8900c3a15396da5545c24ddc
Gerald <gerald.mwang@gmail.com>
2023-10-23 21:16:59 -0600

    used require to refactor. With corrections.

--- test/headTest.js
+++ test/headTest.js
@@ -34,3 +34,32 @@ In your test/assertEqualTest.js file, you've done a great job importing the asse
 
 Keep going! You're almost there.
 */
+
+/*
+(1) Incorporate Mocha and Chai
+Great job on importing the head function and the assert function from the Chai library. Your test cases are also well structured using Mocha's describe and it functions, and Chai's assert.strictEqual for assertions.
+
+However, there's a small issue with the placement of your second test case. It should be inside the describe block. Currently, your code looks like this:
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5'); 
+});
+The second it block should be inside the describe block, like this:
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
+The commented out code at the bottom of your file seems to be unrelated to the current requirement. If it's not needed, consider removing it to keep your code clean and focused.
+*/
