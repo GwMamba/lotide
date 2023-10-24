d7f511c465d3f524ad4ac5fcaf16e0b9970f8849
Gerald <gerald.mwang@gmail.com>
2023-10-24 01:47:27 -0600

    The function wil return a slice of the array with the elements taken from the beginning.

---
+++ takeUntil.js
@@ -0,0 +1,66 @@
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (const item of array) {
+    if (!callback(item)) {
+      result.push(item);
+    } else {
+      break;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const areEqual = eqArrays(arr1, arr2);
+  if (areEqual) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Test Cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+ 
+/*
+Expected Output
+[ 1, 2, 5, 7, 2 ]
+--
+[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+*/
+
+/*
+It will take in two parameters as well:
+
+The array to work with
+The callback (which Lodash calls "predicate")
+
+const takeUntil = function(array, callback) {
+  // ...
+}
+
+
+The function will return a "slice of the array with elements taken from the beginning." It should keep going until the callback/predicate returns a truthy value.
+
+To keep things simple, the callback should only be provided one value: The item in the array.
+*/
