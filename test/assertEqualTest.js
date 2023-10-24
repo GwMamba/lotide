f994c79b96e93bb9b17e99feda7bf300393024c8
Gerald <gerald.mwang@gmail.com>
2023-10-23 20:31:40 -0600

    refactored code using the require and module.export functionality.

---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+
+
+
+
+// Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, 25);
+assertEqual("Alberta", "Ontario")
+assertEqual("X56", "X56")
