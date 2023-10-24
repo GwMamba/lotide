5217bc2f7a795c71028c33c1400808a06042e009
Gerald <gerald.mwang@gmail.com>
2023-10-23 18:21:54 -0600

    Implement assertObjectsEqual which will take in two objects and console.log an appropriate message to the console with notes.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -61,3 +61,16 @@ eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject); // => false
 
 assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
 assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
+
+/*
+However, there's a small issue in your assertObjectEqual function. You're using the inspect function from the 'util' module, but you're not actually using it to print the objects.
+
+Here's the relevant part of your code:
+
+if (eqObjects(actual, expected)) {
+  console.log((`✅✅✅ Assertion Passed: ${actual} === ${expected}`));
+} else {
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+}
+You're directly printing actual and expected, which are objects. This will not give you a readable output. Instead, you should use the inspect function to convert these objects to strings before printing them.
+*/
