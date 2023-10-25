52477356852b03ad8d2c5e52b9ef2d31129783b1
Gerald <gerald.mwang@gmail.com>
2023-10-25 14:13:34 -0600

    added notes with corrections.

--- test/middleTest.js
+++ test/middleTest.js
@@ -35,3 +35,32 @@ You might want to change this to:
 
 assert.deepEqual(middle([1, 2, 3]), [2]);
 */
+
+/*
+(1) Refactor with mocha and chai
+Keep going! Your submission seems like it might be missing something. Let's take a look at your code:
+
+In your tailTest.js file, you've correctly imported the assert function from the Chai library. However, you've also defined the tail function in this file, which is not necessary. You should import the tail function from the tail.js file using the require syntax. Replace your tail function definition with this line:
+
+const tail = require('../tail');
+Also, your test cases in tailTest.js are not structured using Mocha's describe and it functions. You should wrap your test cases in these functions to provide a clear description of what each test case is testing. For example:
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+In your middleTest.js file, you've correctly imported the middle function and the assert function from the Chai library. However, your test cases are not correctly comparing the output of the middle function to the expected output. The middle function returns an array, but you're comparing it to individual values. You should compare the entire output array to the expected output array. For example:
+
+it("returns [2] for [1, 2, 3]", () => {
+  assert.deepEqual(middle([1, 2, 3]), [2]);
+});
+Also, you've included some test cases outside of the describe function. All test cases should be included inside the describe function to ensure they are run as part of the test suite. For example:
+
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  // Include other test cases here
+});
+*/
--- test/tailTest.js
+++ test/tailTest.js
@@ -34,3 +34,32 @@ In your test/tailTest.js file, you've redefined the tail function. Instead, you 
 const tail = require('../tail');
 Finally, your test cases in test/tailTest.js are not correct. The tail function returns an array, but you're comparing it to a single value. You should compare the first element of the returned array to the expected value. For example, instead of assertEqual(tail([5,6,7]), 5);, you should have assertEqual(tail([5,6,7])[0], 6);.
 */
+
+/*
+(1) Refactor with mocha and chai
+Keep going! Your submission seems like it might be missing something. Let's take a look at your code:
+
+In your tailTest.js file, you've correctly imported the assert function from the Chai library. However, you've also defined the tail function in this file, which is not necessary. You should import the tail function from the tail.js file using the require syntax. Replace your tail function definition with this line:
+
+const tail = require('../tail');
+Also, your test cases in tailTest.js are not structured using Mocha's describe and it functions. You should wrap your test cases in these functions to provide a clear description of what each test case is testing. For example:
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+In your middleTest.js file, you've correctly imported the middle function and the assert function from the Chai library. However, your test cases are not correctly comparing the output of the middle function to the expected output. The middle function returns an array, but you're comparing it to individual values. You should compare the entire output array to the expected output array. For example:
+
+it("returns [2] for [1, 2, 3]", () => {
+  assert.deepEqual(middle([1, 2, 3]), [2]);
+});
+Also, you've included some test cases outside of the describe function. All test cases should be included inside the describe function to ensure they are run as part of the test suite. For example:
+
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  // Include other test cases here
+});
+*/
