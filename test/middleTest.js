d6d48e7468831c428c452ff7721ddbb38c14df97
Gerald <gerald.mwang@gmail.com>
2023-10-25 08:03:24 -0600

    Refactor the middle.js file to use modules. possible error somewhere.

---
+++ test/middleTest.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 2);
+  });
+  it("returns 3 and 4 for [1, 2, 3, 4, 5, 6]", () => {
+    assert.strictEqual(middle([1, 2, 3, 4, 5, 6]), 3, 4);
+  });
+});
+
+const assertArraysEqual = require('../middle');
+
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
