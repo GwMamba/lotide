01e250a9002c21051804aa0b5937d67ac57cc662
Gerald <gerald.mwang@gmail.com>
2023-10-28 17:51:58 -0600

    fixed my import abilities with require. Final v4. to be revisited to tackle flatten.

--- index.js
+++ index.js
@@ -22,3 +22,10 @@ const without = require('./without');
 module.exports = {
   head, tail, middle, assertArraysEqual, assertEqual, assertObjectsEqual, countLetters, countOnly, eqArrays, eqObjects, findKey, findKeyByValue, /*flatten,*/ letterPositions, takeUntil, without 
 };
+
+/*
+(1) Implement object shorthand
+Great job on your code! You've correctly used the ES6 property value shorthand in your module.exports object. This is exactly what the requirements were asking for.
+
+One thing I noticed is that you've commented out the flatten function. If this function is part of your project, you should uncomment it so it can be exported with the rest of your functions. If it's not part of your project, you can remove the commented out code to keep your file clean and easy to read.
+*/
