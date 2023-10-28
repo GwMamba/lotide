14fab9d1abaffd5f716ed90956d8c1e049aa7f22
Gerald <gerald.mwang@gmail.com>
2023-10-28 17:39:42 -0600

    lotide functional. Final version 2. to be revisited.

--- index.js
+++ index.js
@@ -1,42 +1,4 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
-
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  takeUntil: takeUntil,
-  without: without 
+  head, tail, middle, assertArraysEqual, assertEqual, assertObjectsEqual, countLetters, countOnly, eqArrays, eqObjects, findKey, findKeyByValue, flatten, letterPositions, takeUntil, without 
 };
-
-// Example 1
-// Can export a number (not common, but JS won't stop me!)
-module.exports = 5;
-
-// Example 2
-// Can export an object (quite common!)
-module.exports = { 
-  firstName: "Lighthouse",
-  lastName:  "Labs"
-}
-
-// Example 3
-// Can export a function (we did this for all our functions)
-module.exports = function() {
-  return "developers, developers, developers!"; 
-}
-
-// Etc.
